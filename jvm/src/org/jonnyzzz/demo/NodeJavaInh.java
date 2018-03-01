99b48ba3d7f29bd130a7f38b63f5acc1bd98e9d9
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-03-01 14:30:22 +0100

    make example more detailed

--- jvm/src/org/jonnyzzz/demo/NodeJavaInh.java
+++ jvm/src/org/jonnyzzz/demo/NodeJavaInh.java
@@ -4,4 +4,9 @@ public class NodeJavaInh extends NodeJava<String> {
   public NodeJavaInh(String value) {
     super(value);
   }
+
+  @Override
+  public void setValue(String value) {
+
+  }
 }
--- jvm/src/org/jonnyzzz/demo/node.kt
+++ jvm/src/org/jonnyzzz/demo/node.kt
@@ -1,7 +1,12 @@
 package org.jonnyzzz.demo
 
 
-open class NodeKotlin<T>(var value : T)
+open class NodeKotlin<T>(var value : T) {
+  open fun foo(v : T) = Unit
+}
 
-class NodeKotlinInh(value: String) : NodeKotlin<String>(value)
+class NodeKotlinInh(value: String) : NodeKotlin<String>(value){
+
+  override fun foo(v: String) = Unit
+}
 
