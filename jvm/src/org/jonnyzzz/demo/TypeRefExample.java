e7dfa0c8b26991c2c016dd5f116f261830b0f75d
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-03-06 15:56:25 +0100

    typeRefExample
    Signed-off-by: Eugene Petrenko <eugene.petrenko@gmail.com>

---
+++ jvm/src/org/jonnyzzz/demo/TypeRefExample.java
@@ -0,0 +1,19 @@
+package org.jonnyzzz.demo;
+
+import java.util.List;
+import java.util.Map;
+
+public class TypeRefExample {
+  private static <T> T deserialize(TypeRef<T> o) { return null;}
+
+  public static void main(String[] args) {
+
+
+    final Map<String, List<Long>> map =
+            deserialize(new TypeRef<Map<String, List<Long>>>() {});
+
+
+
+
+  }
+}
