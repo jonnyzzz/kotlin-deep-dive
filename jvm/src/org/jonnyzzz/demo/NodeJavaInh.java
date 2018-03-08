a31a45f5eaa77dc4d7e8e5ad40d8c2c99fcb5871
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-03-08 11:10:37 +0100

    yet another example
    Signed-off-by: Eugene Petrenko <eugene.petrenko@gmail.com>

--- jvm/src/org/jonnyzzz/demo/NodeJavaInh.java
+++ jvm/src/org/jonnyzzz/demo/NodeJavaInh.java
@@ -9,4 +9,9 @@ public class NodeJavaInh extends NodeJava<String> {
   public void setValue(String value) {
 
   }
+
+  public static void main(String[] args) {
+    NodeJava i = new NodeJavaInh("@jonnyzzz");
+    i.setValue(new Object());
+  }
 }
